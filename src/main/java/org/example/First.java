46785342b584b210f7d47e1adbd425aab969278c
NataliaKaminsky <124137357+NataliaKaminsky@users.noreply.github.com>
2023-12-11 09:37:11 +0200

    Added method1 class First

--- src/main/java/org/example/First.java
+++ src/main/java/org/example/First.java
@@ -3,4 +3,10 @@ public class First {
     public void method(){
         System.out.println("---------");
     }
+
+    public void method1(){
+        System.out.println("---------");
+    }
+
+
 }
