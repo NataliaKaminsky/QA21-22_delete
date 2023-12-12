8e7255bbd6a8ca0509267939678abca2ac79805f
NataliaKaminsky <124137357+NataliaKaminsky@users.noreply.github.com>
2023-12-12 18:08:07 +0200

    Added first branch

--- src/main/java/org/example/Second.java
+++ src/main/java/org/example/Second.java
@@ -4,4 +4,8 @@ public class Second {
     public void methodBranch(){
         System.out.println("first branch created");
     }
+
+    public void methodBranch1(){
+        System.out.println("first branch created+method");
+    }
 }
